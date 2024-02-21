21a1bc6f02aa84a0ade142c078039dcb77bd6770
YOUNESSE25 <younessehaouas@gmail.com>
2024-02-21 12:03:55 -0800

    AirBnB_clone_v2

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-from uuid import uuid4
 import models
+from uuid import uuid4
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
+
+Base = declarative_base()
+
 
 class BaseModel:
+    """Defines the BaseModel class.
+
+    Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+
+    # id = Column(String(60), primary_key=True, nullable=False)
+    # created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    # updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Defines the BaseModel class. """
+        """Initialize a new BaseModel.
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
