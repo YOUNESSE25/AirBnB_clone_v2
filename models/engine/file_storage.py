3cd194322db78a9395a2848d308d9c20c61042d6
YOUNESSE25 <younessehaouas@gmail.com>
2024-02-21 16:54:30 -0800

    AirBnB_clone_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,9 +16,21 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    # def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        # return FileStorage.__objects
+        dictFinal = {}
+        if not cls:
+            return self.__objects
+        else:
+            dct = self.__objects
+            for ki in dct:
+                block = ki.replace('.', ' ')
+                block = shlex.split(block)
+                if (block[0] == cls.__name__):
+                    dictFinal[ki] = self.__objects[ki]
+            return (dictFinal)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,13 +47,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -48,3 +61,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Dilete element"""
+        if obj:
+            del self.__objects[("{}.{}".format(type(obj).__name__, obj.id))]
