7a55bd37260f729e1926cb43c097d63b2b512baa
YOUNESSE25 <younessehaouas@gmail.com>
2024-03-24 15:03:28 +0000

    web_flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application:'''
+from flask import Flask
+
+
+app = Flask(__name__)
+'''Flask app'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''home'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''HBNB page'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''C is fun'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
